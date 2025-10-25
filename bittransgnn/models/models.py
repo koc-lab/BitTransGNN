1747dd450808a7e7ef117d39560fe73295d8a487
EralpK <eralpkumbasar@gmail.com>
2025-10-25 10:00:21 -0400

    Add model classes integrating PTQ baselines (OS/OS+) into the proposed architecture

--- bittransgnn/models/models.py
+++ bittransgnn/models/models.py
@@ -153,3 +153,187 @@ class BitTransformerStudent(nn.Module):
             soft_pred = torch.log(soft_pred)
         return pred, soft_pred, logits
 
+# Defining the models relevant to the integration of PTQ baselines (OS/OS+) with BitTransGNN
+
+class BitTransGNNOS(nn.Module):
+    def __init__(self, bert_model, classifier, pretrained_model='bert-base-uncased', joint_training=True, quantize_gcn=False, gcn_num_states=2, nb_class=20, lmbd=0.7, gcn_layers=2, n_hidden=200, dropout=0.5, regression=False):
+        super(BitTransGNNOS, self).__init__()
+        self.lmbd = lmbd
+        self.joint_training = joint_training
+        self.nb_class = nb_class
+        self.tokenizer = AutoTokenizer.from_pretrained(pretrained_model)
+        #self.bert_model = AutoModel.from_pretrained(pretrained_model)
+        self.bert_model = bert_model
+        #self.feat_dim = list(self.bert_model.modules())[-2].out_features
+        #self.classifier = nn.Linear(self.feat_dim, nb_class)
+        self.classifier = classifier
+        self.feat_dim = classifier.in_features
+        self.gcn = GCNTorch(in_features=self.feat_dim, hidden_features=n_hidden, out_features=nb_class, dropout=dropout, quantize=quantize_gcn, num_states=gcn_num_states)
+        self.regression = regression
+
+    def forward(self, graph_data, idx, temperature=1, recompute_bert=False):
+        if self.joint_training:
+            input_ids, attention_mask, token_type_ids = graph_data.input_ids[idx], graph_data.attention_mask[idx], graph_data.token_type_ids[idx]
+            if self.training or recompute_bert:
+                cls_feats = self.bert_model(input_ids, attention_mask, token_type_ids=token_type_ids)[0][:, 0]
+                graph_data.cls_feats[idx] = cls_feats
+            else:
+                cls_feats = graph_data.cls_feats[idx]
+        else:
+            cls_feats = graph_data.cls_feats[idx]
+        cls_logit = self.classifier(cls_feats)
+        gcn_logit = self.gcn(x=graph_data.cls_feats, adj=graph_data.adj_sparse)[idx]
+        logits = {"cls_logit": cls_logit.clone(), "gcn_logit": gcn_logit.clone()}
+        if self.regression:
+            gcn_pred, cls_pred = gcn_logit, cls_logit
+            gcn_soft_pred, cls_soft_pred = gcn_logit, cls_logit
+            pred = gcn_pred * self.lmbd + cls_pred * (1 - self.lmbd)
+            soft_pred = gcn_soft_pred * self.lmbd + cls_soft_pred * (1 - self.lmbd)
+        else:
+            cls_pred = nn.Softmax(dim=1)(cls_logit/temperature)
+            gcn_pred = nn.Softmax(dim=1)(gcn_logit/temperature)
+            cls_soft_pred = nn.Softmax(dim=1)(cls_logit/temperature)
+            gcn_soft_pred = nn.Softmax(dim=1)(gcn_logit/temperature)
+            pred = (gcn_pred+1e-10) * self.lmbd + cls_pred * (1 - self.lmbd)
+            soft_pred = (gcn_soft_pred+1e-10) * self.lmbd + cls_soft_pred * (1 - self.lmbd)
+            #pred = torch.log(pred)
+        return pred, soft_pred, logits
+
+class BitTransformerOS(nn.Module):
+    def __init__(self, bert_model, classifier, pretrained_model='bert-base-uncased', nb_class=20, regression=False):
+        super(BitTransformerOS, self).__init__()
+        self.tokenizer = AutoTokenizer.from_pretrained(pretrained_model)
+        self.bert_model = bert_model
+        #self.classifier = nn.Linear(self.feat_dim, nb_class)
+        self.classifier = classifier
+        try:
+            self.feat_dim = self.classifier.in_features
+        except AttributeError:
+            self.feat_dim = self.classifier["weight"].shape[1]
+        self.regression = regression
+
+    def forward(self, input_ids, attention_mask, token_type_ids=None):
+        cls_feats = self.bert_model(input_ids, attention_mask, token_type_ids=token_type_ids)[0][:, 0]
+        cls_logit = self.classifier(cls_feats)
+        logits = {"cls_logit": cls_logit.clone()}
+        if self.regression:
+            pred = cls_logit
+        else:
+            pred = nn.Softmax(dim=1)(cls_logit)
+            pred = torch.log(pred)
+        return pred, logits
+
+class BitTransformerStudentOS(nn.Module):
+    def __init__(self, bert_model, classifier, pretrained_model='bert-base-uncased', nb_class=20, regression=False):
+        super(BitTransformerStudentOS, self).__init__()
+        self.tokenizer = AutoTokenizer.from_pretrained(pretrained_model)
+        #self.bert_model = AutoModel.from_pretrained(pretrained_model)
+        self.bert_model = bert_model
+        self.feat_dim = classifier.in_features
+        #self.classifier = nn.Linear(self.feat_dim, nb_class)
+        self.classifier = classifier
+        self.regression = regression
+
+    def forward(self, graph_data, idx, temperature=1):
+        input_ids, attention_mask, token_type_ids = graph_data.input_ids[idx], graph_data.attention_mask[idx], graph_data.token_type_ids[idx]
+        cls_feats = self.bert_model(input_ids, attention_mask, token_type_ids=token_type_ids)[0][:, 0]
+        cls_logit = self.classifier(cls_feats)
+        logits = {"cls_logit": cls_logit.clone()}
+        if self.regression:
+            pred, soft_pred = cls_logit, cls_logit
+        else:
+            pred = nn.Softmax(dim=1)(cls_logit)
+            pred = torch.log(pred)
+            soft_pred = nn.Softmax(dim=1)(cls_logit/temperature)
+            soft_pred = torch.log(soft_pred)
+        return pred, soft_pred, logits
+    
+
+from transformers import (
+    AutoConfig,
+    AutoModel,
+    PretrainedConfig,
+    AutoModelForSequenceClassification,
+    BertForSequenceClassification,
+    AutoTokenizer,
+)
+from transformers.modeling_outputs import SequenceClassifierOutput
+
+class BertForSeqClsNoPooler(BertForSequenceClassification):
+    def __init__(self, config):
+        super().__init__(config)
+        # make the pooler a no-op (so we don't mistakenly use it)
+        #self.bert.pooler = nn.Identity()
+        self.pooler = nn.Identity()
+
+    def forward(self, input_ids=None, attention_mask=None, token_type_ids=None, labels=None, **kwargs):
+        out = self.bert(input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids, **kwargs)
+        cls = out[0][:, 0]  # match your training
+        logits = self.classifier(cls)
+        loss = None
+        if labels is not None:
+            if self.num_labels == 1:
+                loss_fct = nn.MSELoss()
+                loss = loss_fct(logits.view(-1), labels.view(-1))
+            else:
+                loss_fct = nn.CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+        return SequenceClassifierOutput(loss=loss, logits=logits)
+
+from quant_transformer_os.quantization import Quantizer
+
+class BitTransGNNOSAlt(nn.Module):
+    def __init__(self, bert_model, classifier, pretrained_model='bert-base-uncased', joint_training=True, quantize_gcn=False, gcn_num_states=2, nb_class=20, lmbd=0.7, gcn_layers=2, n_hidden=200, dropout=0.5, regression=False, 
+                 w_qconfig=None, a_qconfig=None, qoutput=True):
+        super(BitTransGNNOSAlt, self).__init__()
+        self.lmbd = lmbd
+        self.joint_training = joint_training
+        self.nb_class = nb_class
+        self.tokenizer = AutoTokenizer.from_pretrained(pretrained_model)
+        #self.bert_model = AutoModel.from_pretrained(pretrained_model)
+        self.bert_model = bert_model
+        self.w_qconfig = w_qconfig
+        self.a_qconfig = a_qconfig
+        self.qoutput = qoutput
+        if self.w_qconfig:
+            self.cls_post_act_fake_quantize = Quantizer(None, a_qconfig)
+        #self.feat_dim = list(self.bert_model.modules())[-2].out_features
+        #self.classifier = nn.Linear(self.feat_dim, nb_class)
+        self.classifier = classifier
+        if self.qoutput:
+            self.logits_post_act_fake_quantize = Quantizer(None, a_qconfig)
+        self.feat_dim = classifier.in_features
+        self.gcn = GCNTorch(in_features=self.feat_dim, hidden_features=n_hidden, out_features=nb_class, dropout=dropout, quantize=quantize_gcn, num_states=gcn_num_states)
+        self.regression = regression
+
+    def forward(self, graph_data, idx, temperature=1, recompute_bert=False):
+        if self.joint_training:
+            input_ids, attention_mask, token_type_ids = graph_data.input_ids[idx], graph_data.attention_mask[idx], graph_data.token_type_ids[idx]
+            if self.training or recompute_bert:
+                cls_feats = self.bert_model(input_ids, attention_mask, token_type_ids=token_type_ids)[0][:, 0]
+                graph_data.cls_feats[idx] = cls_feats
+            else:
+                cls_feats = graph_data.cls_feats[idx]
+        else:
+            cls_feats = graph_data.cls_feats[idx]
+        if self.w_qconfig:
+            cls_feats = self.cls_post_act_fake_quantize(cls_feats, None, 1)
+        cls_logit = self.classifier(cls_feats)
+        if self.qoutput:
+            cls_logit = self.logits_post_act_fake_quantize(cls_logit, None, 1)
+        gcn_logit = self.gcn(x=graph_data.cls_feats, adj=graph_data.adj_sparse)[idx]
+        logits = {"cls_logit": cls_logit.clone(), "gcn_logit": gcn_logit.clone()}
+        if self.regression:
+            gcn_pred, cls_pred = gcn_logit, cls_logit
+            gcn_soft_pred, cls_soft_pred = gcn_logit, cls_logit
+            pred = gcn_pred * self.lmbd + cls_pred * (1 - self.lmbd)
+            soft_pred = gcn_soft_pred * self.lmbd + cls_soft_pred * (1 - self.lmbd)
+        else:
+            cls_pred = nn.Softmax(dim=1)(cls_logit/temperature)
+            gcn_pred = nn.Softmax(dim=1)(gcn_logit/temperature)
+            cls_soft_pred = nn.Softmax(dim=1)(cls_logit/temperature)
+            gcn_soft_pred = nn.Softmax(dim=1)(gcn_logit/temperature)
+            pred = (gcn_pred+1e-10) * self.lmbd + cls_pred * (1 - self.lmbd)
+            soft_pred = (gcn_soft_pred+1e-10) * self.lmbd + cls_soft_pred * (1 - self.lmbd)
+            #pred = torch.log(pred)
+        return pred, soft_pred, logits
